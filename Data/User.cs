9ee94ad2c14f5ab7246434896ab560523ce87ac0
PhilippeMallari <mallari.philippe@gmail.com>
2024-10-16 12:44:16 -0600

    reading from a file, creating lists and storing into them, creating the email validation page

---
+++ Data/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppPhil.Data
+{
+    public class User
+    {
+        // id, email, course
+        int id;
+        string email;
+        string course;
+
+        public int Id { get => id; set => id = value; }
+        public string Email { get => email; set => email = value; }
+        public string Course { get => course; set => course = value; }
+
+        public User(int id, string email, string course)
+        {
+            this.id = id;
+            this.email = email;
+            this.course = course;
+        }
+    }
+}
---
+++ Data/UserManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppPhil.Data
+{
+    public class UserManager
+    {
+        string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\..\\..\\..\\Resources\\res\\Users.txt");
+
+        public static List<User> users = new List<User>();
+
+        public UserManager()
+        {
+            // loading the data when the object is created
+            populateUsers();
+        }
+
+        private void populateUsers()
+        {
+            User user;
+
+            foreach(string line in File.ReadAllLines(filePath))
+            {
+                string[] parts = line.Split(',');
+                int id = int.Parse(parts[0]);
+                string email = parts[1];
+                string course = parts[2];
+
+                // user object
+                user = new User(id, email, course);
+
+                // add user object into the list 'users'
+                users.Add(user);
+            }
+        }
+
+        public static List<User> GetUsers()
+        {
+            // return users list
+            return users;
+        }
+
+    }
+}
